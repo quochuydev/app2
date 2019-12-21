d3c9156abb21c0f52cb117fba22127ce13a4f6f8
quochuydev <quochuy.dev@gmail.com>
2019-12-21 22:45:15 +0700

    update

--- server/server.js
+++ server/server.js
@@ -5,11 +5,10 @@ const port = process.env.PORT || 5000;
 
 console.log('NODE_ENV: ', process.env.NODE_ENV)
 console.log('PORT: ', process.env.PORT)
-
 if (process.env.NODE_ENV == 'production') {
-	app.use(express.static('client/build'));
+	app.use(express.static(path.join(__dirname, '../client/build')));
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.send(express.static(path.join(__dirname, '../client/build/index.html')));
 	});
 }
 
