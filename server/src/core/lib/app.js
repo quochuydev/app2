327db14d0c7447a551a4eba195ddabb76550f4f1
quochuydev <quochuy.dev@gmail.com>
2019-12-22 04:12:57 +0700

    update

--- server/src/core/lib/app.js
+++ server/src/core/lib/app.js
@@ -7,12 +7,9 @@ const PORT = process.env.PORT || 5000;
 
 const App = {
   init: () => {
-    if (process.env.NODE_ENV == 'production') {
-      app.use(express.static(path.join(__dirname, '../client/build')));
-      app.get('/site*', (req, res) => {
-        res.send(express.static(path.join(__dirname, '../client/build/index.html')));
-      });
-    }
+    // if (process.env.NODE_ENV == 'production') {
+      // app.use(express.static(path.join(__dirname, '../client/build')));
+    // }
     Mongoose.load();
     Mongoose.connect()
     .then(db => {
