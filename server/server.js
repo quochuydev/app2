e5c616225ef85c299bceed25433a02800926fba3
quochuydev <quochuy.dev@gmail.com>
2019-12-21 22:29:32 +0700

    update

--- server/server.js
+++ server/server.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express();
 const port = process.env.PORT || 5000;
 
-console.log(process.env.NODE_ENV)
+console.log('NODE_ENV: ', process.env.NODE_ENV)
+console.log('PORT: ',process.env.PORT)
 
 app.get('/', (req, res) => {
 	res.json({ test: false })
+})
+
+app.get('/api/customers', (req, res) => {
+	res.json({ customers: 'test customers' })
 })
 
 app.listen(port, () => {
