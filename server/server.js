899ffd1a6af6e8b78a16a6c8723c8a97cd573652
quochuydev <quochuy.dev@gmail.com>
2019-12-21 21:54:03 +0700

    folder name

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 5000;
+
+console.log(process.env.NODE_ENV)
+
+app.get('/', (req, res) => {
+	res.json({ test: false })
+})
+
+app.listen(port, () => {
+	console.log('running port: ' + port)
+})
