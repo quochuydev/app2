cdcbafa018fd0a8b1d5dc4c0544b0b431cd5f663
quochuydev <quochuy.dev@gmail.com>
2019-12-21 22:41:15 +0700

    update

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
 console.log('NODE_ENV: ', process.env.NODE_ENV)
-console.log('PORT: ',process.env.PORT)
+console.log('PORT: ', process.env.PORT)
+
+if (process.env.NODE_ENV == 'production') {
+	app.use(express.static('client/build'));
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 app.get('/', (req, res) => {
 	res.json({ test: false })
