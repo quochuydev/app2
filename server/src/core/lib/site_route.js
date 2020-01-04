e6238f276bb39788608a0969cd871b71906e3b31
quochuydev <quochuy.dev@gmail.com>
2020-01-05 01:20:08 +0700

    update

--- server/src/core/lib/site_route.js
+++ server/src/core/lib/site_route.js
@@ -3,7 +3,7 @@ var router = express.Router();
 let path = require('path');
 
 router.get('/*', (req, res) => {
-  res.sendFile(path.resolve('../client/public/index.html'));
+  res.sendFile(path.resolve('../client/build/index.html'));
 });
 
 module.exports = router;
