db4998f58f0f86fe4fd2c4727ba48f35ab4fda3a
quochuydev <quochuy.dev@gmail.com>
2020-01-19 19:17:51 +0700

    update app client

--- client/src/views/Admin/App/index.jsx
+++ client/src/views/Admin/App/index.jsx
@@ -81,7 +81,9 @@ function App(props) {
         onCancel={() => setIsShowWoocommerceAppModal(false)}
       >
         <Form>
-          <Form.Item label="Shop URL">{(<Input name="wp_host" onChange={onChange} style={{ width: '100%' }} defaultValue={dataWoocommerce.wp_host} />)}</Form.Item>
+          <Form.Item label="Shop URL">
+            <Input name="wp_host" onChange={onChange} style={{ width: '100%' }} defaultValue={dataWoocommerce.wp_host} />
+          </Form.Item>
         </Form>
         <a href={buildLinkWoocommerce}>{buildLinkWoocommerce}</a>
       </Modal>
