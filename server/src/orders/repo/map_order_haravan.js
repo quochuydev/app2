15fc6d390eb61d0dba47bc96f688a57706efed28
quochuydev <quochuy.dev@gmail.com>
2020-01-20 14:49:04 +0700

    change name folder order

--- server/src/orders/repo/map_order_haravan.js
+++
@@ -1,9 +0,0 @@
-const MapOrderHaravan = {
-  gen(order_hrv){
-    let order = {};
-
-    return order;
-  }
-}
-
-module.exports = MapOrderHaravan;
--- server/src/orders/routes/orders.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const { Schema } = mongoose;
-
-const OrderSchema = new Schema({
-  code: { type: String, default: null },
-})
-
-mongoose.model('Order', OrderSchema);
