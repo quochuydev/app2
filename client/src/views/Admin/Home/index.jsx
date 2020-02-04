f690905684926b2a35dcaa7eda17957d3690482d
quochuydev <quochuy.dev@gmail.com>
2020-02-04 15:53:03 +0700

    update client alert

--- client/src/views/Admin/Home/index.jsx
+++ client/src/views/Admin/Home/index.jsx
@@ -38,19 +38,20 @@ function Home() {
     }
     setTimeout(() => {
       setAlert({ showAlert: false });
-    }, 1000);
-    clearTimeout();
+      clearTimeout();
+    }, 3000);
   }
   function showLoading(timeout = 1000) {
     setIsProcessing(true);
     setTimeout(() => {
       setIsProcessing(false);
+      clearTimeout();
     }, timeout);
-    clearTimeout();
   }
   return (
     <Content style={{ padding: '0 24px', minHeight: 280 }}>Content
       <button onClick={() => showMessage(false, 'messageSuccess')}>show mess</button>
+      <button onClick={() => showMessage(true, 'message failed')}>show mess failed</button>
       <button onClick={() => showLoading()}>show loading</button>
       <Alert messageFailed={messageFailed} messageSuccess={messageSuccess} error={isError} showAlert={showAlert} />
     </Content>
