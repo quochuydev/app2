da569648f8ddb552b0b3be380db1e21f65dbd296
quochuydev <quochuy.dev@gmail.com>
2020-02-07 11:23:43 +0700

    update customer controller

--- server/src/customers/routes/customers.js
+++ server/src/customers/routes/customers.js
@@ -1,8 +1,4 @@
-const path = require('path');
-const customerController = require('../controllers/customers');
 const { list, sync, create, update, importExcel, exportExcel } = require('../controllers/customers');
-const { syncCustomersHaravan, syncCustomersShopify, syncCustomersWoo } = require('../business/customers');
-const logger = require(path.resolve('./src/core/lib/logger'));
 
 const router = ({ app }) => {
   app.post('/api/customers/list', list);
