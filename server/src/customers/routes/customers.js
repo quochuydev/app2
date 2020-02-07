914389f1eb60c905dd638106e2a4f8ac4ae130c1
quochuydev <quochuy.dev@gmail.com>
2020-02-07 11:23:03 +0700

    update customer controller

--- server/src/customers/routes/customers.js
+++ server/src/customers/routes/customers.js
@@ -1,38 +1,17 @@
 const path = require('path');
 const customerController = require('../controllers/customers');
+const { list, sync, create, update, importExcel, exportExcel } = require('../controllers/customers');
 const { syncCustomersHaravan, syncCustomersShopify, syncCustomersWoo } = require('../business/customers');
 const logger = require(path.resolve('./src/core/lib/logger'));
 
 const router = ({ app }) => {
-  app.post('/api/customers/list', customerController.list);
-  app.post('/api/customers/create', customerController.create);
-  app.put('/api/customers/:id', customerController.update);
-  app.get('/api/customers/sync', customerController.sync);
-  app.post('/api/customers/import', customerController.import);
-  app.post('/api/customers/export', customerController.export);
-  app.post('/api/customers/sync', async (req, res) => {
-    try {
-      res.json({ error: false });
-      await sync();
-    } catch (error) {
-      logger(error)
-      res.status(400).send({ error: true });
-    }
-  })
+  app.post('/api/customers/list', list);
+  app.post('/api/customers/create', create);
+  app.put('/api/customers/:id', update);
+  app.post('/api/customers/import', importExcel);
+  app.post('/api/customers/export', exportExcel);
+  app.post('/api/customers/sync', sync)
 }
 
 module.exports = router;
 
-
-let sync = async () => {
-  await syncCustomersHaravan();
-  await syncCustomersWoo();
-  await syncCustomersShopify();
-}
-
-let test = async () => {
-  await syncCustomersHaravan();
-  await syncCustomersShopify();
-  await syncCustomersWoo();
-}
-// test();
