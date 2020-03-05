ee0be51798bd0e5c0e4e3100e37aee64a40e21f1
quochuydev <quochuy.dev@gmail.com>
2020-03-05 20:10:51 +0700

    update

--- server/src/products/repo/map_product_woo.js
+++ server/src/products/repo/map_product_woo.js
@@ -1,9 +1,14 @@
 const MapProductWoo = {
   gen(product_woo, url) {
     let product = {
-      type: 'haravan',
+      type: 'woocommerce',
       id: product_woo.id,
       created_at: product_woo.created_at,
+      title: product_woo.name,
+      created_at: product_woo.date_created,
+      updated_at: product_woo.date_modified,
+      variations: product_woo.variations,
+      images: product_woo.images,
       url: url,
       detail: product_woo
     };
