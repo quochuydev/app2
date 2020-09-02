35763d4715f2b712201188118ead5e8bb15226bc
quochuydev <quochuy.dev@gmail.com>
2020-09-02 20:15:18 +0700

    UPDATE POS

--- src/products/controllers/products.js
+++ src/products/controllers/products.js
@@ -5,18 +5,24 @@ const { _parse } = require(path.resolve('./src/core/lib/query'));
 
 let { syncProductsHaravan, syncProductsShopify, syncProductsWoo } = require('../business/products');
 
-let sync = async (req, res) => {
+let Controller = {};
+
+Controller.sync = async (req, res) => {
   try { await syncProductsHaravan(); } catch (err) { console.log(err) }
   try { await syncProductsShopify(); } catch (err) { console.log(err) }
   try { await syncProductsWoo(); } catch (err) { console.log(err) }
   res.json({ error: false })
 }
 
-let list = async (req, res) => {
+Controller.list = async (req, res) => {
   let { limit, skip, criteria } = _parse(req.body);
   let count = await ProductMD.count(criteria);
   let products = await ProductMD.find(criteria).sort({ number: -1, created_at: -1 }).skip(skip).limit(limit).lean(true);
   res.json({ error: false, count, products })
 }
 
-module.exports = { sync, list }
+Controller.importProducts = async function ({ }) {
+  return;
+}
+
+module.exports = Controller;
--- src/products/routes/products.js
+++ src/products/routes/products.js
@@ -1,8 +1,17 @@
-const { list, sync } = require('../controllers/products')
+const { list, sync, importProducts } = require('../controllers/products')
 
 const router = ({ app }) => {
   app.post('/api/products/sync', sync);
   app.post('/api/products/list', list);
+  app.post('/api/products/import', function (req, res, next) {
+    importProducts({})
+      .then(result => {
+        res.json(result);
+      })
+      .catch(error => {
+        next(error);
+      })
+  });
 }
 
 module.exports = router;
