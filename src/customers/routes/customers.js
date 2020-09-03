d7e4cd6d0af02e7e669cc1268cf14c440266293a
quochuydev <quochuy.dev@gmail.com>
2020-09-03 10:31:20 +0700

    UPDATE POS

--- src/customers/routes/customers.js
+++ src/customers/routes/customers.js
@@ -1,22 +1,11 @@
 const { list, sync, create, update, importExcel, exportExcel } = require('../controllers/customers');
-const multer = require('multer');
-
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'uploads')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.originalname)
-  }
-});
-
-const upload = multer({ storage: storage });
+const { uploadToDisk } = require(path.resolve('./src/core/middlewares/upload'));
 
 const router = ({ app }) => {
   app.post('/api/customers/list', list);
   app.post('/api/customers/create', create);
   app.put('/api/customers/:_id', update);
-  app.post('/api/customers/import', upload.single('file'), function (req, res, next) {
+  app.post('/api/customers/import', uploadToDisk.single('file'), function (req, res, next) {
     res.json({
       file: req.file.path
     });
