c645b4bf82012758ccb8dec45bbbe695c8f166de
quochuydev <quochuy.dev@gmail.com>
2020-09-03 16:21:30 +0700

    UPDATE product

--- src/products/controllers/products.js
+++ src/products/controllers/products.js
@@ -25,10 +25,36 @@ Controller.list = async (req, res) => {
 Controller.importProducts = async function ({ file }) {
   let filePath = path.resolve(file);
   let headers = [
-    { header: 'SKU', key: 'sku' },
-    { header: 'Tên sản phẩm', key: 'name' },
-    { header: 'Ngày tạo', key: 'created_at' },
+    { header: 'Tên', key: 'title' },
+    { header: 'Mô tả', key: 'body_html' },
+    { header: 'Trích dẫn', key: 'handle' },
+    { header: 'Hãng', key: 'title' },
+    { header: 'Loại sản phẩm', key: 'product_type' },
+    { header: 'Tags', key: 'tags' },
+    { header: 'Hiển thị', key: 'published' }, //published_scope:'global' && published_at
+    { header: 'Thuộc tính 1', key: 'option1.name' },
+    { header: 'Giá trị thuộc tính 1', key: 'option1.value' },
+    { header: 'Thuộc tính 2', key: 'option2.name' },
+    { header: 'Giá trị thuộc tính 2', key: 'option2.value' },
+    { header: 'Thuộc tính 3', key: 'option3.name' },
+    { header: 'Giá trị thuộc tính 3', key: 'option3.value' },
+    { header: 'Mã phiên bản sản phẩm', key: 'sku' },
+    { header: 'Khối lượng', key: 'grams' },
+    { header: 'Số lượng tồn kho', key: 'inventory_advance.qty_onhand' }, // qty_onhand/qty_avaiable/qty_incoming/qty_commited
+    { header: 'Đặt hàng khi hết hàng', key: 'inventory_policy' }, // "continue"/"deny"
     { header: 'Giá', key: 'price' },
+    { header: 'Giá so sánh', key: 'compare_at_price' },
+    { header: 'Có giao hàng không?', key: 'requires_shipping' }, // true/false
+    { header: 'Variant Taxable', key: 'taxable' },
+    { header: 'Barcode', key: 'barcode' },
+    { header: 'Link hình', key: 'image_src' },
+    { header: 'Mô tả hình', key: 'image_alt' },
+    { header: 'Danh mục', key: 'vendor' },
+    { header: 'Danh mục EN', key: 'vendor_en' },
+    { header: 'Ảnh biến thể', key: 'variant.image_id' },
+    { header: 'Ngày tạo', key: 'created_at' },
+    { header: 'Ngày cập nhật', key: 'updated_at' },
+    { header: 'Không áp dụng khuyến mãi', key: 'not_allow_promotion' }
   ]
   let data = await ExcelLib.loadFile({ filePath, headers })
   console.log(data)
