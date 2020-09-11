33760d03d268375ad5032f69b02f75f8fa4924d5
quochuydev <quochuy.dev@gmail.com>
2020-09-11 17:09:27 +0700

    UPDATE

--- src/core/lib/express.js
+++ src/core/lib/express.js
@@ -21,10 +21,6 @@ module.exports = (app, db) => {
   if (process.env.NODE_ENV == 'production') {
     console.log(path.resolve('client/build', 'index.html'))
     app.use('/', express.static(path.resolve('client', 'build')));
-    app.get('/', (req, res) => {
-      res.send('embed here');
-      // res.redirect(`${config.frontend_site}/`);
-    });
     app.get('/site/*', (req, res) => {
       res.sendFile(path.resolve('client/build', 'index.html'));
     });
