fcd5f533d2c461688054c024b2be7cd31c2f941b
quochuydev <quochuy.dev@gmail.com>
2020-09-25 09:19:44 +0700

    update

--- client/src/utils/assetProvider.js
+++ client/src/utils/assetProvider.js
@@ -1,7 +1,9 @@
 import logo from '../assets/images/logo.png';
 import puma from '../assets/images/puma.png';
+import myMomo from '../assets/images/my-momo.jpg';
 
 export default {
   logo,
-  puma
+  puma,
+  myMomo
 }
