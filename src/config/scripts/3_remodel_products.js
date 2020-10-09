7f296ae3b07a7bd97485736f0dd42e70f4b339e2
quochuydev <quochuy.dev@gmail.com>
2020-10-09 13:41:31 +0700

    UPDATE

--- src/config/scripts/3_remodel_products.js
+++ src/config/scripts/3_remodel_products.js
@@ -7,6 +7,8 @@ const { CountryModel } = require(path.resolve('./src/core/models/country.js'));
 const { WardModel } = require(path.resolve('./src/core/models/ward.js'));
 const { ProvinceModel } = require(path.resolve('./src/core/models/province.js'));
 const { ProductModel } = require(path.resolve('./src/products/models/product.js'));
+const { VariantModel } = require(path.resolve('./src/products/models/variant.js'));
+
 let _do = require(path.resolve('./src/core/share/_do.lib.share.js'))
 
 Mongoose.connect()
@@ -14,18 +16,19 @@ Mongoose.connect()
     console.log('connect mongo success');
 
     await Promise.all([
-      importWard()
+      // remodelProduct(),
+      remodelVariant(),
     ])
 
     console.log('done')
     process.exit(0)
   })
   .catch(err => {
-    console.log(err)
+    console.log(err);
     console.log('connect mongo fail');
   })
 
-async function importWard() {
+async function remodelProduct() {
   await ProductModel.find({}).cursor().eachAsync(async doc => {
     for (let i = 0; i < doc.variants.length; i++) {
       doc.variants[i].price_original = 0;
@@ -33,5 +36,11 @@ async function importWard() {
     }
     await ProductModel.update({ id: doc.id }, { $set: { variants: doc.variants } })
     console.log(doc.id, doc.handle, doc.title);
+  }, { parallel: 10 });
+}
+
+async function remodelVariant() {
+  await VariantModel.find({}).cursor().eachAsync(async doc => {
+    await VariantModel.update({ id: doc.id }, { $set: { price_original: doc.price } })
   }, { parallel: 10 });
 }
